0636543015c11451971b04ced36aee2608b90e32
Stephen-Beck <stephenbeck89@outlook.com>
2025-08-13 14:49:19 -0400

    Add project files.

---
+++ CustomQueue.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6._3
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+    public class CustomQueue<T>
+    {
+        private int size;
+        private Node<T> Front { get; set; }
+        private Node<T> Rear { get; set; }
+
+        public CustomQueue()
+        {
+            Front = null;
+            Rear = null;
+            size = 0;
+        }
+
+        private bool IsEmpty()
+        {
+            return size == 0;
+        }
+
+        public void Enqueue(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (IsEmpty()) Front = newNode;
+            else Rear.Next = newNode;
+
+            Rear = newNode;
+            size++;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Nothing dequeued, queue is empty.");
+                return default;
+            }
+
+            T data = Front.Data;
+            Front = Front.Next;
+            size--;
+            return data;
+        }
+
+        public T ViewFirst()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty.\n");
+                return default;
+            }
+
+            return Front.Data;
+        }
+
+        public T ViewLast()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty.\n");
+                return default;
+            }
+
+            return Rear.Data;
+        }
+
+        public void Display()
+        {
+            if (IsEmpty()) Console.WriteLine("Queue is empty.\n");
+            else
+            {
+                Console.Write("Current Queue: ");
+                Node<T> temp = Front;
+                while (temp != null)
+                {
+                    Console.Write(temp.Data + " ");
+                    temp = temp.Next;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+You are developing a program to manage a call queue of customers using the Queue  in C#. 
+The program creates a queue of callers and demonstrates the functionality of enqueueing elements 
+into the queue and iterating over the elements and dequeuing.
+Use linked lists. 
+*/
+
+namespace Assignment_6._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomQueue<string> myQueue = new();
+
+            myQueue.Display(); //empty
+            myQueue.Enqueue("Stephen");
+            myQueue.Enqueue("Eduardo");
+            myQueue.Enqueue("Deepali");
+            myQueue.Display();
+            Console.WriteLine($"--Dequeued {myQueue.Dequeue()}\n");
+            myQueue.Display();
+            Console.WriteLine($"--Dequeued {myQueue.Dequeue()}\n");
+            Console.WriteLine($"--Dequeued {myQueue.Dequeue()}\n");
+            Console.WriteLine($"--Dequeued {myQueue.Dequeue()}\n");
+            myQueue.Enqueue("Jerome");
+            myQueue.Enqueue("Angel");
+            myQueue.Enqueue("Brandon");
+            myQueue.Display();
+            Console.WriteLine($"First in queue: {myQueue.ViewFirst()}\n");
+            Console.WriteLine($"Last in queue: {myQueue.ViewLast()}\n");
+        }
+    }
+}
